9dfa3b3c75f03b9b8e2dab808cf06a5f94040f86
Mamun Hossain <mamuncse19@gmail.com>
2021-04-02 16:11:14 +0600

    project has been installed successfully

---
+++ storage/framework/views/156b38d9dcbdc279a329504ddca9aea1c1ed45ca.php
@@ -0,0 +1,97 @@
+<?php $__env->startSection('content'); ?>
+    <div class="p-5">
+        <div class="text-center">
+            <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+        </div>
+
+        <form class="user" method="POST" action="<?php echo e(route('register')); ?>">
+            <?php echo csrf_field(); ?>
+            <div class="form-group">
+                <input type="text" class="form-control form-control-user <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="name" placeholder="First Name">
+                <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                <span class="invalid-feedback" role="alert">
+               <strong><?php echo e($message); ?></strong>
+        </span>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control form-control-user <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="email"
+                       placeholder="Email Address">
+                <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                <span class="invalid-feedback" role="alert">
+               <strong><?php echo e($message); ?></strong>
+        </span>
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-6 mb-3 mb-sm-0">
+                    <input type="password" class="form-control form-control-user <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" name="password"
+                           placeholder="Password">
+                    <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                    <span class="invalid-feedback" role="alert">
+               <strong><?php echo e($message); ?></strong>
+        </span>
+                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                </div>
+                <div class="col-sm-6">
+                    <input type="password" class="form-control form-control-user" name="password_confirmation" placeholder="Repeat Password">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary btn-user btn-block">
+                <?php echo e(__('Register Account')); ?>
+
+            </button>
+        </form>
+        <hr>
+        <div class="text-center">
+            <a class="small" href="<?php echo e(route('password.request')); ?>">Forgot Password?</a>
+        </div>
+        <div class="text-center">
+            <a class="small" href="<?php echo e(route('login')); ?>">Already have an account? Login!</a>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.auth', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\xampp-7.4.14\htdocs\mediusware\resources\views/auth/register.blade.php ENDPATH**/ ?>
